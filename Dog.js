7a6378932339993006298a102a2b6cad77f76064
duchoan <truongduchoan000@gmail.com>
2022-07-11 23:29:28 +0700

    Dog eat Cat

--- Dog.js
+++ Dog.js
@@ -1,4 +1,4 @@
-var chalk = require('chalk');
+//var chalk = require('chalk');
 
 function Dog(name) {
     this.name = name
@@ -10,7 +10,8 @@ Dog.prototype.eat = function (cat) {
 }
 
 Dog.prototype.sayHi = function () {
-    console.log('Hi! I am a dog. My name is ' + chalk.blue(this.name));
+    console.log('Hi! I am a dog. My name is ' +//chalk.blue
+    (this.name));
 }
 
 module.exports = Dog;
